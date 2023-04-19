5645a5eafb6cf79b975717b397611b1769455e10
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-04-19 21:40:07 +0300

    connect wallet

---
+++ constants/index.ts
@@ -0,0 +1,218 @@
+export const contract_address = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [],
+    name: "BONUS_PERCENTAGE",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "BONUS_REFERRALS",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "REFERRAL_PERCENTAGE",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "REGISTRATION_FEE",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "userAddress",
+        type: "address",
+      },
+    ],
+    name: "getUserBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "userAddress",
+        type: "address",
+      },
+    ],
+    name: "getUserLevel",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "userAddress",
+        type: "address",
+      },
+    ],
+    name: "getUserReferrals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "userAddress",
+        type: "address",
+      },
+    ],
+    name: "getUserReferrer",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "levelUp",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_referrer",
+        type: "address",
+      },
+    ],
+    name: "register",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "users",
+    outputs: [
+      {
+        internalType: "address",
+        name: "referrer",
+        type: "address",
+      },
+      {
+        internalType: "uint8",
+        name: "totalReferrals",
+        type: "uint8",
+      },
+      {
+        internalType: "uint8",
+        name: "level",
+        type: "uint8",
+      },
+      {
+        internalType: "uint256",
+        name: "earnings",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserProvider, ethers } from "ethers";
+import { abi } from "../constants/index";
 
-const index = () => {
-  return <div></div>;
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+const Home = () => {
+  const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [walletProvider, setWalletProvider] = useState<string>();
+
+  const connectWallet = async () => {
+    try {
+      let signer = null;
+
+      let provider;
+      if (window.ethereum == null) {
+        console.log("MetaMask not installed; using read-only defaults");
+        provider = ethers.getDefaultProvider("localhost");
+      } else {
+        provider = new ethers.BrowserProvider(window.ethereum);
+        // await provider.send("eth_requestAccounts", []);
+
+        console.log(provider);
+        signer = await provider.getSigner();
+        setWalletProvider(signer.address);
+      }
+
+      setWalletConnected(false);
+      console.log(CONTRACT_ADDRESS, abi, signer);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    walletConnected && console.log(walletProvider);
+    // Listen for disconnect event
+    window.ethereum.on("disconnect", () => {
+      console.log("Wallet disconnected");
+      setWalletConnected(false);
+    });
+  }, []);
+  return (
+    <div>
+      {walletProvider ? (
+        <h1>Wallet Connected: {walletProvider}</h1>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
 };
 
-export default index;
+export default Home;
